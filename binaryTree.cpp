4cee4b12b9b9917bddfa57c0eee0855ddbc7c1d2
bimmbim <bimabaraja76@gmail.com>
2025-06-26 12:05:46 +0700

    alokasi dan set nilai node baru

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -26,7 +26,9 @@ class BinaryTree {
             cout << "Masukkan nilai: ";
             cin >> x;
 
-            
+            Node *newNode = new Node();
+
+            newNode->info = x;
 
             
 
