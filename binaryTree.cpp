2b6240ba21c4c1d7b1f26485fde9ec627d771ce4
bimmbim <bimabaraja76@gmail.com>
2025-06-26 12:08:31 +0700

    preorder traversal dengan validasi

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -81,7 +81,18 @@ class BinaryTree {
             inorder(ptr->rightchild);
         }
 
-        
+        void preorder(Node *ptr) {
+            if (isEmpty()) {
+                cout << "Tree is empty" << endl;
+                return;
+            }
+            if (ptr == nullptr)
+                return;
+
+            cout << ptr->info << " ";
+            preorder(ptr->leftchild);
+            preorder(ptr->rightchild);
+        }
 
         
 
